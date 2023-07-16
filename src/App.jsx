dd5b2eb050fa387ad673a12b42182f6a6630a827
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-16 23:04:06 +0200

    add planet

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import reactLogo from "./assets/jupiter.svg";
 import "./App.css";
 
 function App() {
-
   return (
     <>
       <img src={reactLogo} className="jupiter" alt="React logo" />
+      <div className="planet">
+        <div className="ring"></div>
+      </div>
     </>
   );
 }
