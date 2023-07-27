3c0c9d89f1e90c81765c917bc81ab53cc7fa7d9b
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-27 17:04:26 +0200

    tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 // App.jsx
 import React from "react";
-import SVGPlanet from "./assets/Skelett.svg";
-import Background from "./assets/Hintergrund.svg";
+import skeleton from "./assets/skeleton.svg";
 import ring from "./assets/gradient-ring.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="App">
-        <div className="planet">
-          <img src={SVGPlanet} className="planetX" />
-          <img src={ring} className="ring ring1" draggable="false" />
-          <img src={ring} className="ring ring2" draggable="false" />
-          <img src={ring} className="ring ring3" draggable="false" />
-          <img src={ring} className="ring ring4" draggable="false" />
+      <div id="myDiv">
+        <div>
+          <div className="skeleton-canvas">
+            <img src={skeleton} className="skeletonSvg" />
+            <img src={ring} className="ring ring1" draggable="false" />
+            <img src={ring} className="ring ring2" draggable="false" />
+            <img src={ring} className="ring ring3" draggable="false" />
+            <img src={ring} className="ring ring4" draggable="false" />
+          </div>
         </div>
       </div>
     </>
