2127480ed73769a4c0ee73268e5f661ea551d46e
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-27 18:21:38 +0200

    add img

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@
 import React from "react";
 import skeleton from "./assets/skeleton.svg";
 import ring from "./assets/gradient-ring.svg";
+import scanner from "./assets/scanner.png";
 import "./App.css";
 
 function App() {
+
   return (
     <>
+      <div id="background-left">
+        <img id="centered-image" src={scanner} draggable="false" />
+      </div>
+
       <div id="background-right">
         <div className="skeleton-canvas">
-          <img src={skeleton} className="skeletonSvg" />
+          <img src={skeleton} className="skeletonSvg" draggable="false" />
           <img src={ring} className="ring ring1" draggable="false" />
           <img src={ring} className="ring ring2" draggable="false" />
           <img src={ring} className="ring ring3" draggable="false" />
