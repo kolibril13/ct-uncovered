e13a7730ffd547483f7e79b10889f94d01f9754c
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-27 15:50:59 +0200

    add gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 // App.jsx
+import React from "react";
 import SVGPlanet from "./assets/Skelett.svg";
+import Background from "./assets/Hintergrund.svg";
 import ring from "./assets/gradient-ring.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="planet">
-        <img src={SVGPlanet} className="planetX" />
-        <img src={ring} className="ring ring1" draggable="false"/> 
-        <img src={ring} className="ring ring2" draggable="false" />
-        <img src={ring} className="ring ring3" draggable="false"/>
-        <img src={ring} className="ring ring4" draggable="false"/>
-
-    </div>
+    <>
+      <div className="App">
+        <div className="planet">
+          <img src={SVGPlanet} className="planetX" />
+          <img src={ring} className="ring ring1" draggable="false" />
+          <img src={ring} className="ring ring2" draggable="false" />
+          <img src={ring} className="ring ring3" draggable="false" />
+          <img src={ring} className="ring ring4" draggable="false" />
+        </div>
+      </div>
+    </>
   );
 }
 
