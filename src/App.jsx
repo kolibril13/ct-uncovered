1e7a35b3af7ec05672eed3e13c0616bbeefe23ef
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-27 18:34:10 +0200

    first rotation idea

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
 // App.jsx
-import React from "react";
+import React, { useState, useRef } from "react";
 import skeleton from "./assets/skeleton.svg";
 import ring from "./assets/gradient-ring.svg";
 import scanner from "./assets/scanner.png";
 import "./App.css";
 
 function App() {
+  const [angle, setAngle] = useState(0);
+  const [isRotating, setIsRotating] = useState(false);
+  const imageRef = useRef(null);
+
+  const handleMouseDown = (event) => {
+    setIsRotating(true);
+  };
+
+  const handleMouseUp = (event) => {
+    setIsRotating(false);
+  };
+
+  const handleMouseMove = (event) => {
+    if (!isRotating) return;
+
+    const rect = imageRef.current.getBoundingClientRect();
+    const x = event.clientX - rect.left - rect.width / 2;
+    const y = event.clientY - rect.top - rect.height / 2;
+    const newAngle = Math.atan2(y, x) * (180 / Math.PI);
+    setAngle(newAngle+180);
+  };
 
   return (
     <>
       <div id="background-left">
-        <img id="centered-image" src={scanner} draggable="false" />
+        <img
+          id="centered-image"
+          src={scanner}
+          style={{ transform: `rotate(${angle}deg)` }}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseLeave={handleMouseUp}
+          onMouseMove={handleMouseMove}
+          ref={imageRef}
+          draggable="false" 
+        />
       </div>
 
       <div id="background-right">
