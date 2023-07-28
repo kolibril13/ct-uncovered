01e9dd55e98aa721f8bbb1c1c41d34311329a188
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-28 09:18:40 +0200

    red green logic

--- src/Skeleton.jsx
+++ src/Skeleton.jsx
@@ -4,15 +4,43 @@ import skeleton from "./assets/skeleton.svg";
 import ring from "./assets/gradient-ring.svg";
 import "./Skeleton.css";
 
-function Skeleton({slices, setSlices, level, setLevel}) {
+function Skeleton({ slices, setSlices, level, setLevel }) {
+  const handleClick = (index) => {
+    if (!slices[index]) {
+      console.log("red");
+    }
+    if (slices[index]) {
+      console.log("green");
+    }
+  };
   return (
     <div id="background-right">
       <div className="skeleton-canvas">
         <img src={skeleton} className="skeletonSvg" draggable="false" />
-        <img src={ring} className="ring ring1" draggable="false" />
-        <img src={ring} className="ring ring2" draggable="false" />
-        <img src={ring} className="ring ring3" draggable="false" />
-        <img src={ring} className="ring ring4" draggable="false" />
+        <img
+          src={ring}
+          className="ring ring1"
+          draggable="false"
+          onClick={() => handleClick(0)}
+        />
+        <img
+          src={ring}
+          className="ring ring2"
+          draggable="false"
+          onClick={() => handleClick(1)}
+        />
+        <img
+          src={ring}
+          className="ring ring3"
+          draggable="false"
+          onClick={() => handleClick(2)}
+        />
+        <img
+          src={ring}
+          className="ring ring4"
+          draggable="false"
+          onClick={() => handleClick(3)}
+        />
       </div>
     </div>
   );
