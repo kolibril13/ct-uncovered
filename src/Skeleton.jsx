348a47c43424c9b74da1c9d0ef40147508d47008
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-28 13:10:33 +0200

    add checks and crosses

--- src/Skeleton.jsx
+++ src/Skeleton.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Skeleton.css";
 
 function CheckComponent() {
@@ -10,12 +10,32 @@ function CrossComponent() {
 }
 
 function Skeleton() {
+  const [checks, setChecks] = useState([]);
+  const [crosses, setCrosses] = useState([]);
+
+  const addCheck = () => {
+    const id = new Date().getTime();
+    setChecks([...checks, id]);
+    setTimeout(() => {
+      setChecks(checks => checks.filter(check => check !== id));
+    }, 1000);
+  };
+
+  const addCross = () => {
+    const id = new Date().getTime();
+    setCrosses([...crosses, id]);
+    setTimeout(() => {
+      setCrosses(crosses => crosses.filter(cross => cross !== id));
+    }, 1000);
+  };
+
+  console.log(checks, crosses)
   return (
     <div id="background-right">
-      <button className="button">Check!</button>
-      <button className="button">Cross!</button>
-      <CrossComponent />
-      <CheckComponent />
+      <button className="button" onClick={addCheck}>Check!</button>
+      <button className="button" onClick={addCross}>Cross!</button>
+      {checks.map(id => <CheckComponent key={id} />)}
+      {crosses.map(id => <CrossComponent key={id} />)}
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>
+  // </StrictMode> 
 );
