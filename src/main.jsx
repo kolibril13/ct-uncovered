32c961ae9260a31d8798b14c73abbdadf07b99b6
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-31 14:39:43 +0200

    add fastclick

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+import initReactFastclick from 'react-fastclick';
+initReactFastclick();
+
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   // <StrictMode>
