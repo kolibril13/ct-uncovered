3c98cab9796e6bed925ed870de0ce2b2d68bf5ef
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-01 10:31:05 +0200

    rotate image

--- src/MatrixImage.jsx
+++ src/MatrixImage.jsx
@@ -1,31 +1,35 @@
 import React, { useRef, useEffect } from "react";
-import "./MatrixImage.css"
+import "./MatrixImage.css";
 
 function MatrixImage({ matrix }) {
-    const canvasRef = useRef();
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-      const imgData = ctx.createImageData(matrix.length, matrix[0].length);
-      for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[i].length; j++) {
-          const idx = (i + j * matrix.length) * 4;
-          const value = matrix[i][j];
-          imgData.data[idx] = value; // Red channel
-          imgData.data[idx + 1] = value; // Green channel
-          imgData.data[idx + 2] = value; // Blue channel
-          imgData.data[idx + 3] = 255; // Alpha channel (fully opaque)
-        }
-      }
-  
-      ctx.putImageData(imgData, 0, 0);
-    }, [matrix]);
-  
-    return (
-      // <canvas  ref={canvasRef} width={matrix.length} height={matrix[0].length} />
-         <canvas className="myprojectionimg" ref={canvasRef} width={matrix.length} height={matrix[0].length} />
+  const canvasRef = useRef();
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
 
-    );
-  }
-  
-  export default MatrixImage;
+    const imgData = ctx.createImageData(matrix.length, matrix[0].length);
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        const idx = (i + j * matrix.length) * 4;
+        const value = matrix[i][j];
+        imgData.data[idx] = 255-value; // Red channel
+        imgData.data[idx + 1] = 255-value; // Green channel
+        imgData.data[idx + 2] = 255-value; // Blue channel
+        imgData.data[idx + 3] = 255; // Alpha channel (fully opaque)
+      }
+    }
+
+    ctx.putImageData(imgData, 0, 0);
+  }, [matrix]);
+
+  return (
+    <canvas
+      className="myprojectionimg"
+      ref={canvasRef}
+      width={matrix.length}
+      height={matrix[0].length}
+    />
+  );
+}
+
+export default MatrixImage;
