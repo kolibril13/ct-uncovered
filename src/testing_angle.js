3403951c9784a6a1ffc760db57b61c8f2435fc76
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-07 17:16:06 +0200

    tweak angle testing

--- src/testing_angle.js
+++ src/testing_angle.js
@@ -1,22 +1,34 @@
-const rangeModulo = (start, end) => {
-    if (start > end) {
-      [start, end] = [end, start];
+function rangeModulo(a, b) {
+    if (a > b) {
+        [a, b] = [b, a];
+      }
+    var result = [];
+    if (a <= b && (b - a) <= 10) {
+        for (var i = a; i <= b; i++) {
+            result.push(i);
+        }
+    } else if (a > b || (b - a) > 10) {
+        for (var i = a; i < 21; i++) {
+            result.push(i);
+        }
+        for (var i = 0; i <= b; i++) {
+            result.push(i);
+        }
     }
-    let result = [];
-    if ((end - start) <= 10 || (end - start) > 10) {
-      for (let i = start; i <= end; i++) {
-        result.push(i);
-      }
-    } else {
-      for (let i = start; i <= 20; i++) {
-        result.push(i);
-      }
-      for (let i = 1; i <= end; i++) {
-        result.push(i);
-      }
-    }
-    return result;  
-  };
+    return result;
+}
   
 
-  console.log(rangeModulo(20,1)) 
+  console.log(rangeModulo(13, 17));  // Output: [13, 14, 15, 16, 17]
+  console.log(rangeModulo(17, 13));  // Output: [13, 14, 15, 16, 17]
+  console.log(rangeModulo(17, 2));   // Output: [17, 18, 19, 20, 0, 1, 2]
+  console.log(rangeModulo(2, 17));   // Output: [17, 18, 19, 20, 0, 1, 2]
+
+// but for the second one I get
+
+[
+    2,  3,  4,  5,  6,  7,  8,  9, 10, 11,
+   12, 13, 14, 15, 16, 17, 18, 19, 20,  0,
+    1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
+   11, 12, 13, 14, 15, 16, 17
+ ]
