de41048e42deb01972d1b709e719763fb8500b31
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-08 20:20:21 +0200

    re-add intro

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -6,7 +6,11 @@ function Intro() {
         <h1>CT uncovered</h1>
         <p>A webapp to explore image reconstruction in a ct scanner.</p>
         {/* <p>Made for the Summer of Maths Exposition 2023.</p> */}
-        {/* <button>Start</button> */}
+
+
+        {/* <h1>How it works:</h1> */}
+
+        <button>Start</button>
       </div>
     </div>
   );
