b0b4ece1fe149d57166598397be591e6f6f7d2ed
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-08 17:16:57 +0200

    add start

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -3,8 +3,9 @@ function Intro() {
     <div className="intro">
       <div className="logo">
         <p>Jan-Hendrik Müller and MOIN MOTION present</p>
-        <h1>CT uncovered.</h1>
+        <h1>CT uncovered</h1>
         <p>A webapp to expolore image reconstuction in a ct scanner</p>
+        <button>Start</button>
       </div>
     </div>
   );
