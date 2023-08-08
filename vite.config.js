ef2bf2a6462264e2584bdb0a4275685194734cd6
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-08 21:25:00 +0200

    rename

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/okapi-vite-react-app/',
+  base: '/ct-uncovered/',
   plugins: [react()],
 })
