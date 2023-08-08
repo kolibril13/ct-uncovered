7fd83dcb75f786c82a707ef95014196df1452324
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-08 17:26:31 +0200

    some3

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -4,7 +4,8 @@ function Intro() {
       <div className="logo">
         <p>Jan-Hendrik Müller and MOIN MOTION present</p>
         <h1>CT uncovered</h1>
-        <p>A webapp to explore image reconstruction in a ct scanner</p>
+        <p>A webapp to explore image reconstruction in a ct scanner.</p>
+        <p>Made for the Summer of Maths Exposition 2023.</p>
         <button>Start</button>
       </div>
     </div>
