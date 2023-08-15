40943bb85f300671c77554b63ba910b1dcde7237
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-15 14:48:44 +0200

    update modal

--- src/InfoModal.jsx
+++ src/InfoModal.jsx
@@ -2,22 +2,26 @@ const InfoModal = ({ show, close }) => {
   return show ? (
     <div className="intro">
       <div className="modal-content">
-        <div className="logo">
-          <h1>CT uncovered</h1>
-          <p>A webapp to explore image reconstruction in a CT scanner.</p>
-          <p>
-            {" "}
-            Concept and implementation: <br /> Jan-Hendrik Müller{" "}
-          </p>
-          <p>
-            {" "}
-            Design: <br /> MOIN MOTION{" "}
-          </p>
+        {/* <div className="logo right-aligned "> */}
+        <h1>CT uncovered</h1>
+        <p>A webapp to explore image reconstruction in a CT scanner. <br></br>
+        Select angles on the right side with the mouse, and then try to find the corresponding slice on the right
+        </p>
+        <p>
+          You can find a whole animation video on the topic here: <br />
+          VIDEO IS COMMING SOON.
+        </p>
+        <p>
+          {" "}
+          Concept and implementation: <br /> Jan-Hendrik Müller <br /> <br />
+          Design: <br /> MOIN MOTION
+        </p>
 
-          <p> TODO: Add here another paragraph with some more infos </p>
-          <button onClick={close}>Close </button>
-        </div>
+        <p> This is an Open Source Project, you can find the whole code here:   <a href="https://github.com/kolibril13/ct-uncovered" >https://github.com/kolibril13/ct-uncovered</a> </p>
+        <button onClick={close}>Close </button>
+
       </div>
+      {/* </div> */}
     </div>
   ) : null;
 };
