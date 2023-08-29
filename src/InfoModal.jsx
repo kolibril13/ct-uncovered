840993eed01920193a522e8ad7569da433df883f
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-08-29 18:18:59 +0200

    add tweet

--- src/InfoModal.jsx
+++ src/InfoModal.jsx
@@ -4,22 +4,34 @@ const InfoModal = ({ show, close }) => {
       <div className="modal-content">
         {/* <div className="logo right-aligned "> */}
         <h1>CT uncovered</h1>
-        <p>A webapp to explore image reconstruction in a CT scanner. <br></br>
-        Select angles on the right side with the mouse, and then try to find the corresponding slice on the right
-        </p>
         <p>
-          You can find a whole animation video on the topic here: <br />
-          VIDEO IS COMMING SOON.
+          A webapp to explore image reconstruction in a CT scanner. <br></br>
+          Select angles on the left side with the mouse, and then try to find
+          the corresponding slice on the right.
         </p>
+
+        <p>
+          {" "}
+          Learn more about CT scans in this thread: <br />{" "}
+          <a href="https://twitter.com/kolibril13/status/1696554901977899140">
+            https://twitter.com/kolibril13/status/1696554901977899140
+          </a>{" "}
+        </p>
+
         <p>
           {" "}
           Concept and implementation: <br /> Jan-Hendrik Müller <br /> <br />
           Design: <br /> MOIN MOTION
         </p>
 
-        <p> This is an Open Source Project, you can find the whole code here:   <a href="https://github.com/kolibril13/ct-uncovered" >https://github.com/kolibril13/ct-uncovered</a> </p>
+        <p>
+          {" "}
+          This is an Open Source Project, you can find the whole code here:{" "}
+          <a href="https://github.com/kolibril13/ct-uncovered">
+            https://github.com/kolibril13/ct-uncovered
+          </a>{" "}
+        </p>
         <button onClick={close}>Close </button>
-
       </div>
       {/* </div> */}
     </div>
--- src/Outro.jsx
+++ src/Outro.jsx
@@ -5,7 +5,7 @@ function Outro({ onStart }) {
     <div className="intro">
       <div className="logo">
         <h1>Well done!</h1>
-        <p>If you liked this webapp, feel free to retweet this announcement tweet: --insert link here-- </p>
+        <p>If you liked this webapp, feel free to retweet this announcement tweet: <a href="https://twitter.com/kolibril13/status/1696554938225053714">https://twitter.com/kolibril13/status/1696554938225053714</a>   </p>
         <button onClick={onStart}>Restart</button>
 
       </div>
